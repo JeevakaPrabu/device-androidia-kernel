ad663d5b4e6574782c51bde2d221f2a578ad91f0
Sakari Ailus <sakari.ailus@linux.intel.com>
2017-03-22 06:03:23 +0530

    media: v4l: Add 32-long vector YUV format define
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -556,6 +556,10 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_YUV444M v4l2_fourcc('Y', 'M', '2', '4') /* 24  YUV444 planar */
 #define V4L2_PIX_FMT_YVU444M v4l2_fourcc('Y', 'M', '4', '2') /* 24  YVU444 planar */
 
+/* Vector YUV formats */
+#define V4L2_PIX_FMT_YYUV422_V32 v4l2_fourcc('y', 'V', '3', '2')
+#define V4L2_PIX_FMT_YYUV420_V32 v4l2_fourcc('y', '0', '3', '2')
+
 /* Bayer formats - see http://www.siliconimaging.com/RGB%20Bayer.htm */
 #define V4L2_PIX_FMT_SBGGR8  v4l2_fourcc('B', 'A', '8', '1') /*  8  BGBG.. GRGR.. */
 #define V4L2_PIX_FMT_SGBRG8  v4l2_fourcc('G', 'B', 'R', 'G') /*  8  GBGB.. RGRG.. */
